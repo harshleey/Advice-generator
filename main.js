7961834062a2e5ce45c3416456dd9a83f21bf0b1
Adekunle Fatima <logun.fateem@gmail.com>
2022-07-27 14:13:13 +0100

    New commit

--- main.js
+++ main.js
@@ -3,24 +3,15 @@ document.querySelector('button').addEventListener('click', rollDice)
 async function rollDice(){
   const url = 'https://api.adviceslip.com/advice';
   const harshTag = "#" 
-  document.querySelector('.dice').style.transform = "(360deg)"
+  
   const data = await fetch (url)
   const response = await data.json()
+  // const transforming 
   document.querySelector('h1').innerText = response.slip.advice
   document.querySelector('span').innerText = harshTag + response.slip.id
-
-  // fetch(url)
-  //     .then(res => res.json()) // parse response as JSON
-  //     .then(data => {
-  //       console.log(data.slip.advice)
-  //       document.querySelector('h1').innerText = data.slip.advice
-  //       document.querySelector('span').innerText = harshTag + data.slip.id
-  //     })
-  //     .catch(err => {
-  //         console.log(`error ${err}`)
-  //     }); 
+  document.querySelector('.dice').style.transform = "rotate(360deg)"
 }
 
 setInterval(async() => {
   await rollDice()
-}, 2000)
+}, 3000)
